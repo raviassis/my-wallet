3d6b15e40044b9fae940e029fe3b37566a3beeec
raviassis <ravi.g.assis@gmail.com>
2020-02-25 14:49:46 -0300

    Configurando variaveis de ambiente

--- my-wallet-client/server.js
+++ my-wallet-client/server.js
@@ -13,4 +13,4 @@ res.sendFile(path.join(__dirname+'/dist/my-wallet-client/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3001);
--- my-wallet-client/src/environments/environment.prod.ts
+++ my-wallet-client/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'apiUrl',
+  ambiente: 'produção',
 };
